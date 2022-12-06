0fcc8262db0a54f90d5132c51c75a2b69e8dc2ff
10632656 <114419190+10632656@users.noreply.github.com>
2022-12-06 16:05:56 +0000

    init file

---
+++ Src/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+sqldb = SQLAlchemy()
+DB_NAM = "database.sqldb"
+
+
+def create_webapp():
+    webapp = Flask(__name__)
+    # Ideally for production secret key is not added
+   # webapp.config['SECRET_KEY'] = 'abcd abcdefg'
+   # webapp.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAM}'
+    sqldb.init_app(webapp)
+    return webapp
---
+++ main.py
@@ -0,0 +1,8 @@
+from src import create_webapp
+
+app = create_webapp()
+
+
+# 
+if __name__ =='__main__':
+    app.run(debug=True)
