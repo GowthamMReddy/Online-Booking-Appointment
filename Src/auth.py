0087c0ba00cf4d58afd4de0fa6f8968963815b42
likhith <likhithpindi17@gmail.com>
2022-12-09 23:26:49 +0000

    updated flask

--- Src/auth.py
+++ Src/auth.py
@@ -8,11 +8,18 @@ app = Flask(__name__)
 @app.route('/home', methods= ['GET','POST'])
 def home():
     First_Name = request.form['firstName']
-    path = 'system/sys@//localhost/xe'
+    LastName = request.form['lastName']
+    Email = request.form['email']
+    Phone = request.form['phone']
+    Password = request.form["password"]
+
+
+    
+    path = 'system/sys@//localhost:1521/xe'
     cx_Oracle.init_oracle_client(lib_dir=r"C:\oraclexe\app\oracle\instantclient_21_7")
     connection = cx_Oracle.connect(path) 
     cursor = connection.cursor()
-    cursor.execute("""insert into login__ values(:dep, :dep1)""",dep=First_Name,dep1='lihkth')
+    cursor.execute("""insert into Register values(:firstname, :lastname, :email, :phone, :password)""",firstname = First_Name,lastname = LastName, email = Email, phone = Phone, password = Password )
     connection.commit()
 
 
