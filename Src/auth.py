09885d37f482c8c80fda2fdfe6f63a5588d052c6
likhith <likhithpindi17@gmail.com>
2022-12-10 19:18:25 +0000

    added Login_router

--- Src/auth.py
+++ Src/auth.py
@@ -5,8 +5,8 @@ import cx_Oracle
 
 app = Flask(__name__)
 
-@app.route('/home', methods= ['GET','POST'])
-def home():
+@app.route('/Register', methods= ['GET','POST'])
+def Register():
     First_Name = request.form['firstName']
     LastName = request.form['lastName']
     Email = request.form['email']
@@ -21,16 +21,32 @@ def home():
     cursor = connection.cursor()
     cursor.execute("""insert into Register values(:firstname, :lastname, :email, :phone, :password)""",firstname = First_Name,lastname = LastName, email = Email, phone = Phone, password = Password )
     connection.commit()
-
-
-        
     if request.method == 'POST':
          return ' HELLO %s'%First_Name
 
     else:
         return 'erroe'
 
- 
+
+@app.route('/Login', methods = ['POST','GET'])
+def Login():
+    Email = request.form['login_email']
+    Password = request.form['login_password']
+    path = 'system/sys@//localhost:1521/xe'
+    cx_Oracle.init_oracle_client(lib_dir=r"C:\oraclexe\app\oracle\instantclient_21_7")
+    connection = cx_Oracle.connect(path) 
+    cursor = connection.cursor()
+    cursor.execute("""insert into Login values(:email, :password)""",email = Email , password = Password )
+    connection.commit()
+    if request.method == 'POST':
+         return ' HELLO %s'%Email
+
+    else:
+        return 'erroe'
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug = True)
