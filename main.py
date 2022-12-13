fc0f97b83d0e3cb4f31c6ef39734fec60038c5cb
GowthamMReddy <117168448+GowthamMReddy@users.noreply.github.com>
2022-12-13 02:00:06 +0000

    Made Changes for folder structure. Created the Webpage for lottery ticket booking using HTML code.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from src import create_webapp
 
-app = create_webapp()
+from flask import Flask
+from Src import create_webapp
+app= create_webapp()
 
-
-# 
-if __name__ =='__main__':
-    app.run(debug=True)
+if __name__=='__main__':
+    app.run(debug=True, port=8000)
