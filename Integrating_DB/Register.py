d8400eb47d39db8913cc4b91805010ab2f97eb4a
Venkatraoseelam <s.venkatrao66@gmail.com>
2022-12-16 11:41:34 +0000

    adding home page image

--- Integrating_DB/Register.py
+++
@@ -1,8 +0,0 @@
-import cx_Oracle 
-
-path = "system/sys@//localhost/xe"
-cx_Oracle.init_oracle_client(lib_dir=r"C:\oraclexe\app\oracle\instantclient_21_7")
-with cx_Oracle.connect(path) as connection:
-    cursor = connection.cursor()
-    cursor.execute("create table Register (first_name varchar(10),last_name varchar(10),email_id varchar(100),phone number(10),password varchar(10))")
-    
