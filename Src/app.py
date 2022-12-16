dc4fd25faa05cb5fe40a3d9d77a25acda80c8b0d
10632656 <114419190+10632656@users.noreply.github.com>
2022-12-16 12:13:46 +0000

    payment gateway Stripe Integration

---
+++ Src/app.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template, abort, redirect, request
+import stripe
+
+app = Flask(__name__)
+stripe.api_key = os.environ['STRIPE_SECRET_KEY']
+
+
+@app.route('/order/<product_id>', methods=['POST'])
+def order(product_id):
+    if product_id not in products:
+        abort(404)
+
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[
+            {
+                'price_data': {
+                    'product_data': {
+                        'name': products[product_id]['name'],
+                    },
+                    'unit_amount': products[product_id]['price'],
+                    'currency': 'usd',
+                },
+                'quantity': 1,
+            },
+        ],
+        payment_method_types=['card'],
+        mode='payment',
+        success_url=request.host_url + 'order/success',
+        cancel_url=request.host_url + 'order/cancel',
+    )
+    return redirect(checkout_session.url)
