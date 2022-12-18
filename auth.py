d52731acea5e1c7fbdb6c1761abcc13c404d9f9d
GowthamMReddy <117168448+GowthamMReddy@users.noreply.github.com>
2022-12-18 12:57:43 +0000

    'addedfields'

--- auth.py
+++ auth.py
@@ -15,7 +15,7 @@ def register_db():
     Phone = request.form['phone']
     Password = request.form["password"]
     path = 'system/sys@//localhost:1521/xe'
-    cx_Oracle.init_oracle_client(lib_dir=r"C:\oraclexe\app\oracle\instantclient_21_7")
+    cx_Oracle.init_oracle_client(lib_dir=r"C:\oraclexe\app\oracle\instantclient_21_7-20221215T235126Z-001\instantclient_21_7")
     connection = cx_Oracle.connect(path) 
     cursor = connection.cursor()
     try:
@@ -43,7 +43,7 @@ def login_db():
     Login_Email = request.form["login_email"]
     Login_Password = request.form["login_password"]
     path = 'system/sys@//localhost:1521/xe'
-    cx_Oracle.init_oracle_client(lib_dir=r"C:\oraclexe\app\oracle\instantclient_21_7")
+    cx_Oracle.init_oracle_client(lib_dir=r"C:\oraclexe\app\oracle\instantclient_21_7-20221215T235126Z-001\instantclient_21_7")
     connection = cx_Oracle.connect(path) 
     cursor = connection.cursor()
     try:
@@ -67,8 +67,14 @@ def login_db():
 
 @app.route("/user_profile", methods = ['POST', 'GET'])
 def user_profile():
-    
-    return render_template("user_profile.html")
+
+        path = 'system/sys@//localhost:1521/xe'
+        cx_Oracle.init_oracle_client(lib_dir=r"C:\oraclexe\app\oracle\instantclient_21_7-20221215T235126Z-001\instantclient_21_7")
+        connection = cx_Oracle.connect(path) 
+        cursor = connection.cursor()
+        cursor.execute("""select FIRST_NAME from Register """)
+        
+        return render_template("user_profile.html")
     
     
        
